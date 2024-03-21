dbb018593df8b898fa481e489d5e9c2dfdd7b044
Profility <marcuscalpe19@gmail.com>
2024-03-21 22:02:29 +0800

    yml to toml

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+/#!/bin/sh
 # Alacritty Color Export
 # Version 0.1.1
 # github.com/egeesin
@@ -9,7 +9,7 @@
 #
 # Dependencies: grep, sed
 # Usage: ./script.sh
-#        ./script.sh <config yml>
+#        ./script.sh <config toml>
 
 # Function to display error and quit
 die() {
@@ -17,7 +17,7 @@ die() {
   exit 1
 }
 
-DEFAULT_MACOS_CONFIG="$HOME"/.config/alacritty/alacritty.yml
+DEFAULT_MACOS_CONFIG="$HOME"/.config/alacritty/alacritty.toml
 
 # Wal generates a shell script that defines color0..color15
 SRC="$HOME"/.cache/wal/colors.sh
@@ -83,31 +83,33 @@ fi
 # We know $colorX is unset, we set it by sourcing above
 # shellcheck disable=SC2154
 { sed "/^# BEGIN ACE/ r /dev/stdin" "$CFG" > "$tempfile" <<EOF
-colors:
-  primary:
-    background: '$color0'
-    foreground: '$color7'
-  cursor:
-    text:       '$color0'
-    cursor:     '$color7'
-  normal:
-    black:      '$color0'
-    red:        '$color1'
-    green:      '$color2'
-    yellow:     '$color3'
-    blue:       '$color4'
-    magenta:    '$color5'
-    cyan:       '$color6'
-    white:      '$color7'
-  bright:
-    black:      '$color8'
-    red:        '$color9'
-    green:      '$color10'
-    yellow:     '$color11'
-    blue:       '$color12'
-    magenta:    '$color13'
-    cyan:       '$color14'
-    white:      '$color15'
+[colors.primary]
+background = '$color0'
+foreground = '$color7'
+
+[colors.cursor]
+text = '$color0
+cursor = '$color7'
+
+[colors.normal]
+black = '$color0'
+red = '$color1'
+green = '$color2'
+yellow = '$color3'
+blue = '$color4'
+magenta = '$color5'
+cyan = $color6'
+white = '$color7'
+
+[colors.bright]
+black = '$color8'
+red = '$color9'
+green = '$color10'
+yellow = '$color11'
+blue = '$color12'
+magenta = '$color13'
+cyan = '$color14'
+white = '$color15'
 EOF
 } && cat "$tempfile" > "$CFG" \
   && rm "$tempfile"
